12ccfb46576542b9d097f5ace95128341d83be68
vlad.gavanescu <gavanescuvlad@yahoo.com>
2023-09-01 17:34:25 +0300

    Update home text

---
+++ src/components/AnimatedTXT.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const AnimatedTextWord = ({ text }) => {
+  const words = text.split(" ");
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.20, delayChildren: 0.15 * i },
+    }),
+  };
+
+  const child = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 20,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
+
+  return (
+    <motion.h2 className='text-white text-4xl font-permmarker md:text-7xl font-bold overflow-hidden'
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {words.map((word, index) => (
+        <motion.span
+          variants={child}
+          style={{marginRight: "5px"}}
+          key={index}
+        >
+          {word}
+        </motion.span>
+      ))}
+    </motion.h2>
+  );
+};
+
+export default AnimatedTextWord;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import me from '../assets/me2u.jpg'
 import { BiRightArrowAlt } from 'react-icons/bi'
 import { Link } from 'react-scroll'
+import AnimatedTextWord from './AnimatedTXT'
+import AnimatedTextCharacter from './AnimatedTXTChar'
 
 const Home = () => {
+  const [showComponent, setShow] = useState('')
+  useEffect(() => {
+    const delayComp = setTimeout(() => {
+      setShow(true);
+    }, 200);
+    return () => clearTimeout(delayComp);
+  }, [showComponent])
+
   return (
     <div
       name="home"
       className="h-screen w-full ">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-      <div className='visible md:hidden mt-[70%]'>
+        <div className='visible md:hidden mt-[70%]'>
           <img
             className='rounded-2xl mx-auto my-10 w-3/5 md:w-3/5'
             src={me}
             alt="my profile" />
         </div>
         <div className="flex flex-col justify-center h-full">
-          <h2 className='text-white text-4xl font-permmarker md:text-7xl font-bold'>
-            I'm a Front-End Developer
-          </h2>
-          <p className='text-gray-500 py-4 font-bodyfont max-w-md'>
-            Creative front-end developer having a keen eye on design technologies, building responsive websites and web applications.
-            As a front-end developer my skills are based on HTML, CSS, JavaScript plus modern libraries and frameworks
-            (React JS, Tailwind, Bootstrap, MUI), but not limited to these.
-          </p>
+
+          <AnimatedTextWord text={"I'm a Front-End Developer"}></AnimatedTextWord>
+          {showComponent && showComponent ?
+            <div className='text-gray-500 py-4 font-bodyfont max-w-md'>
+              <AnimatedTextCharacter text={"Creative front-end developer having a keen eye on design technologies, building responsive websites and web applications. As a front-end developer my skills are based on HTML, CSS, JavaScript plus modern libraries and frameworks (React JS, Tailwind, Bootstrap, MUI), but not limited to these."}></AnimatedTextCharacter>
+            </div>
+            : <></>
+          }
+
           <div>
             <Link to='portfolio' smooth duration={500}
               className='group text-white font-bodyfont w-fit cursor-pointer px-6 py-4 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-800 via-cyan-500 to-cyan-200 '>
